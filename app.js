1f158f6a7baa472a2f64bda81f16037bfd22c824
RaviPs22 <raviprakashs2203@gmail.com>
2023-10-24 10:38:44 +0000

    code writed

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const Dbpath = path.join(__dirname, "cricketTeam.db");
+const dbConnect = null;
+const mainF1 = async () => {
+  try {
+    dbConnect = await open({
+      filename: Dbpath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3003, () => {
+      console.log("Your Server is Started");
+    });
+  } catch (e) {
+    console.log(`DB Error :${e.message}`);
+    process.exit(1);
+  }
+};
+mainF1();
+app.get("/players/", async (request, response) => {
+  const qweryis = `
+    SELECT 
+    *
+    FROM
+    cricket_team
+    `;
+  const mainC = await dbConnect.get(qweryis);
+  response.send(mainC);
+});
